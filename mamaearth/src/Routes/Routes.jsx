8b5adcdb06788bc3cb46c2ae5588daa2205e5dcd
Mitul Asodariya <70693181+Mitul-778@users.noreply.github.com>
2022-05-06 15:26:15 +0530

    hair page is done

--- mamaearth/src/Routes/Routes.jsx
+++ mamaearth/src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route} from 'react-router-dom'
 import { BabyPage } from '../components/BabyPage/BabyPage'
 import { FacePage } from '../components/FacePage/FacePage'
+import { HairPage } from "../components/HairPage/HairPage";
 
 
 export const AllRoutes = () =>{
@@ -9,6 +10,7 @@ export const AllRoutes = () =>{
         <Routes>
             <Route path="/baby" element={<BabyPage/>}/>
             <Route path="/face" element={<FacePage/>}/>
+            <Route path="/hair" element={<HairPage/>}/>
             {/* <Route path="/face/:id" element={<ProductDetail/>}/> */}
         </Routes>
     )
