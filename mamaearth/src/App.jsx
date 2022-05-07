cd85a98648732475b6116ceacf17a475f5442ca4
gafoor8374 <97434962+gafoor8374@users.noreply.github.com>
2022-05-07 21:23:21 +0530

    signup

---
+++ mamaearth/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { SignUp } from './components/Signin';
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
